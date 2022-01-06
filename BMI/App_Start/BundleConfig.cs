6d43c60c517c9f0cc0db8cb8063994734fc420b4
Amber pan <pan781023@gmail.com>
2022-01-06 15:25:06 +0800

    完成

---
+++ BMI/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace BMI
+{
+    public class BundleConfig
+    {
+        // 如需統合的詳細資訊，請瀏覽 https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        //"~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery/jquery.min.js",
+                        "~/Scripts/bootstrap/js/bootstrap.bundle.min.js",
+                        "~/Scripts/jquery-easing/jquery.easing.min.js",
+                        "~/Scripts/sb-admin-2.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        /*"~/Scripts/jquery.validate*"*/
+                        ));
+
+            // 使用開發版本的 Modernizr 進行開發並學習。然後，當您
+            // 準備好可進行生產時，請使用 https://modernizr.com 的建置工具，只挑選您需要的測試。
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        /*"~/Scripts/modernizr-*"*/
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      /*"~/Scripts/bootstrap.js"*/
+                      ));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/fontawesome-free/css/all.min.css",
+                 "~/Content/css/sb-admin-2.min.css"
+                      //"~/Content/bootstrap.css",
+                      /*"~/Content/site.css"*/
+                      ));
+        }
+    }
+}
---
+++ BMI/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BMI.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Index(double w , double h , string gender ,string User)
+        {
+
+            var hight = h / 100;
+            // BMI 計算，四捨五入小數第二位
+            var BMI =Math.Round(Convert.ToDouble(w /( hight * hight)), 1);
+           //var  BMI = w / h / h;
+            if ((BMI < 19 && gender == "female") || (BMI < 20 && gender == "male"))
+            {
+                ViewBag.result = "體重過輕";
+               // Console.WriteLine("Underweight");
+            }
+            if ((BMI < 19 && BMI <= 24 && gender == "female") || (BMI < 20 && BMI <= 25 && gender == "male"))
+            {
+                ViewBag.result = "健康體位";
+                //if (BMI == 22)
+                //{
+                //    ViewBag.result = "健康體位";
+                //  // Console.WriteLine("Perfect");
+                //}
+                //else
+                //{
+                //    ViewBag.result = "Moderate"; 
+                //   // Console.WriteLine("Moderate");
+                //}
+            }
+            if ((BMI > 24 && BMI <= 29 && gender == "female") || (BMI > 25 && BMI <= 30 && gender == "male"))
+            {
+                ViewBag.result = "輕度肥胖";
+             // Console.WriteLine("Overweight");
+            }
+            if ((BMI > 29 && BMI <= 34 && gender == "female") || (BMI > 30 && BMI <= 35 && gender == "male"))
+            {
+                ViewBag.result = "中度肥胖";
+              // Console.WriteLine("Fat");
+            }
+            if ((BMI > 34 && gender == "female") || (BMI > 35 && gender == "male"))
+            {
+                ViewBag.result = "重度肥胖";
+                //Console.WriteLine("Very Fat");
+            }
+
+            ViewBag.BMI = BMI;
+            ViewBag.user = User;
+            return PartialView("ResultView");
+        }
+
+      
+        public ActionResult Index()
+        {
+
+          
+
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
